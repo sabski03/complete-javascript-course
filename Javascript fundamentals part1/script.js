852068818db79f175052998e6f3aa6c642cf2156
sabski03 <saba27ge@gmail.com>
2025-09-01 19:02:56 +0400

    coding challange #4 completed

--- Javascript fundamentals part1/script.js
+++ Javascript fundamentals part1/script.js
@@ -425,33 +425,116 @@
 
 
 
-// ----- Coding Challenge #3 -----
-//There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+// // ----- Coding Challenge #3 -----
+// //There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+// // Your tasks:
+// // 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+// // 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+// // "Dolphins win the trophy" if Dolphins win, or
+// // "Koalas win the trophy" if Koalas win, or
+// // "Both win the trophy" if their average scores are equal.
+
+// // TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log("Dolphins win the trophy");
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log("Koalas win the trophy");
+// } else {
+//     console.log("Both win the trophy");
+// }
+
+
+
+
+
+
+
+
+
+// // ----- Switch Statemenst -----
+
+// const day = 'Wednesday';
+
+// switch (day) {
+//     case 'Monday':
+//         console.log("Plan my course");
+//         console.log("Go to coding meetup");
+//         break;
+//     case 'Tuesday':
+//         console.log("prepare theory videos");
+//         break;
+//     case 'Wednesday':
+//     case 'Thursday':
+//         console.log("Write code examples");
+//         break;
+//     case 'Friday':
+//         console.log("record videos");
+//         break;
+//     case 'Saturday':
+//     case 'Sunday':
+//         console.log("Enjoy the weekend!");
+//         break;
+//     default:
+//         console.log("Not a valid day!");
+// }
+
+
+
+
+
+
+
+
+
+// // ----- Conditional Operator -----
+
+// const age = 24;
+// age >= 18 ? console.log("I am allowed to drink wine") : console.log("I am not allowed to drink wine");
+
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = "wine";
+// } else {
+//     drink2 = "water";
+// }
+// console.log(drink2);
+
+// console.log(`i like to drink ${age >= 18 ? "wine" : "water"}`)
+
+
+
+
+
+
+
+
+
+// ----- Coding Challenge #4 -----
+
+// Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 // Your tasks:
-// 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
-// 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+// Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+// Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
 
-// "Dolphins win the trophy" if Dolphins win, or
-// "Koalas win the trophy" if Koalas win, or
-// "Both win the trophy" if their average scores are equal.
+// Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+// Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+// TEST DATA: Test with different bill values: 275, 40, and 430
+// HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+// HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
-// TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
-
-const scoreDolphins = (96 + 108 + 89) / 3;
-const scoreKoalas = (88 + 91 + 110) / 3;
-
-if (scoreDolphins > scoreKoalas) {
-    console.log("Dolphins win the trophy");
-} else if (scoreKoalas > scoreDolphins) {
-    console.log("Koalas win the trophy");
-} else {
-    console.log("Both win the trophy");
-}
-
-
-
-
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 15 / 100 : bill * 20 / 100;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${tip + bill}`);
 
 
 
